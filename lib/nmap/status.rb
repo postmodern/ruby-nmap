e042c2b1154d6f0e51af7caba46f558bb17efa6c
postmodern <postmodern.mod3@gmail.com>
2009-11-12 19:00:45 -0800

    Added YARD tags to Nmap::Status.

--- lib/nmap/status.rb
+++ lib/nmap/status.rb
@@ -1,15 +1,32 @@
 module Nmap
   class Status
 
+    # The state of a host
     attr_reader :state
 
+    # The reason of the state
     attr_reader :reason
 
+    #
+    # Creates a new Status object.
+    #
+    # @param [Symbol] state
+    #   The state of a host.
+    #
+    # @param [String] reason
+    #   The reason for the state.
+    #
     def initialize(state,reason)
       @state = state
       @reason = reason
     end
 
+    #
+    # Converts the status to a String.
+    #
+    # @return [String]
+    #   The state.
+    #
     def to_s
       @state.to_s
     end
