8479a043ae999b890d0a0b7510c5cabdaa986041
postmodern <postmodern.mod3@gmail.com>
2009-11-12 18:43:56 -0800

    Added YARD tags to Nmap::Host.

--- lib/nmap/host.rb
+++ lib/nmap/host.rb
@@ -11,10 +11,22 @@ module Nmap
 
     include Enumerable
 
+    #
+    # Creates a new Host object.
+    #
+    # @param [Nokogiri::XML::Node] node
+    #   The XML node that contains the host information.
+    #
     def initialize(node)
       @node = node
     end
 
+    #
+    # Parses the status of the host.
+    #
+    # @return [Status]
+    #   The status of the host.
+    #
     def status
       status = @node.at('status')
 
@@ -24,6 +36,18 @@ module Nmap
       )
     end
 
+    #
+    # Parses each address of the host.
+    #
+    # @yield [addr]
+    #   Each parsed address will be pass to a given block.
+    #
+    # @yieldparam [Address] addr
+    #   A address of the host.
+    #
+    # @return [Host]
+    #   The host.
+    #
     def each_address(&block)
       @node.xpath("address[@addr]").each do |addr|
         address = Address.new(
@@ -37,10 +61,22 @@ module Nmap
       return self
     end
 
+    #
+    # Parses the addresses of the host.
+    #
+    # @return [Array<Host>]
+    #   The addresses of the host.
+    #
     def addresses
       Enumerator.new(self,:each_address).to_a
     end
 
+    #
+    # Parses the MAC address of the host.
+    #
+    # @return [String]
+    #   The MAC address of the host.
+    #
     def mac
       unless @mac
         addr = @node.xpath("address[@addr][@addrtype='mac']").first
@@ -51,6 +87,12 @@ module Nmap
       return @mac
     end
 
+    #
+    # Parses the IPv4 address of the host.
+    #
+    # @return [String]
+    #   The IPv4 address of the host.
+    #
     def ipv4
       unless @ipv4
         addr = @node.xpath("address[@addr][@addrtype='ipv4']").first
@@ -61,6 +103,12 @@ module Nmap
       return @ipv4
     end
 
+    #
+    # Parses the IPv6 address of the host.
+    #
+    # @return [String]
+    #   The IPv6 address of the host.
+    #
     def ipv6
       unless @ipv6
         addr = @node.xpath("address[@addr][@addrtype='ipv6']").first
@@ -71,14 +119,38 @@ module Nmap
       return @ipv6
     end
 
+    #
+    # The IP address of the host.
+    #
+    # @return [String]
+    #   The IPv4 or IPv6 address of the host.
+    #
     def ip
       ipv6 || ipv4
     end
 
+    #
+    # The address of the host.
+    #
+    # @return [String]
+    #   The IP or MAC address of the host.
+    #
     def address
       ip || mac
     end
 
+    #
+    # Parses the hostnames of the host.
+    #
+    # @yield [host]
+    #   Each parsed hostname will be passed to the given block.
+    #
+    # @yieldparam [String] host
+    #   A hostname of the host.
+    #
+    # @return [Host]
+    #   The host.
+    #
     def each_hostname(&block)
       @node.xpath("hostnames/hostname[@name]").each do |host|
         block.call(host['name']) if block
@@ -87,16 +159,40 @@ module Nmap
       return self
     end
 
+    #
+    # Parses the hostnames of the host.
+    #
+    # @return [Array<String>]
+    #   The hostnames of the host.
+    #
     def hostnames
       Enumerator.new(self,:each_hostname).to_a
     end
 
+    #
+    # Parses the OS guessing information of the host.
+    #
+    # @return [OS]
+    #   The OS guessing information.
+    #
     def os
       os = @node.at('os')
 
       return OS.new(os) if os
     end
 
+    #
+    # Parses the scanned ports of the host.
+    #
+    # @yield [port]
+    #   Each scanned port of the host.
+    #
+    # @yieldparam [Port] port
+    #   A scanned port of the host.
+    #
+    # @return [Host]
+    #   The host.
+    #
     def each_port(&block)
       @node.xpath("ports/port").each do |port|
         block.call(create_port(port)) if block
@@ -105,10 +201,28 @@ module Nmap
       return self
     end
 
+    #
+    # Parses the scanned ports of the host.
+    #
+    # @return [Array<Port>]
+    #   The scanned ports of the host.
+    #
     def ports
       Enumerator.new(self,:each_port).to_a
     end
 
+    #
+    # Parses the open ports of the host.
+    #
+    # @yield [port]
+    #   Each open port of the host.
+    #
+    # @yieldparam [Port] port
+    #   An open scanned port of the host.
+    #
+    # @return [Host]
+    #   The host.
+    #
     def each_open_port(&block)
       @node.xpath("ports/port[state/@state='open']").each do |port|
         block.call(create_port(port)) if block
@@ -117,10 +231,28 @@ module Nmap
       return self
     end
 
+    #
+    # Parses the open ports of the host.
+    #
+    # @return [Array<Port>]
+    #   The open ports of the host.
+    #
     def open_ports
       Enumerator.new(self,:each_open_port).to_a
     end
 
+    #
+    # Parses the TCP ports of the host.
+    #
+    # @yield [port]
+    #   Each TCP port of the host.
+    #
+    # @yieldparam [Port] port
+    #   An TCP scanned port of the host.
+    #
+    # @return [Host]
+    #   The host.
+    #
     def each_tcp_port(&block)
       @node.xpath("ports/port[@protocol='tcp']").each do |port|
         block.call(create_port(port)) if block
@@ -129,10 +261,28 @@ module Nmap
       return self
     end
 
+    #
+    # Parses the TCP ports of the host.
+    #
+    # @return [Array<Port>]
+    #   The TCP ports of the host.
+    #
     def tcp_ports
       Enumerator.new(self,:each_tcp_port).to_a
     end
 
+    #
+    # Parses the UDP ports of the host.
+    #
+    # @yield [port]
+    #   Each UDP port of the host.
+    #
+    # @yieldparam [Port] port
+    #   An UDP scanned port of the host.
+    #
+    # @return [Host]
+    #   The host.
+    #
     def each_udp_port(&block)
       @node.xpath("ports/port[@protocol='udp']").each do |port|
         block.call(create_port(port)) if block
@@ -141,27 +291,55 @@ module Nmap
       return self
     end
 
+    #
+    # Parses the UDP ports of the host.
+    #
+    # @return [Array<Port>]
+    #   The UDP ports of the host.
+    #
     def udp_ports
       Enumerator.new(self,:each_udp_port).to_a
     end
 
+    #
+    # Parses the open ports of the host.
+    #
+    # @see each_open_port
+    #
     def each(&block)
       each_open_port(&block)
     end
 
+    #
+    # Converts the host to a String.
+    #
+    # @return [String]
+    #   The address of the host.
+    #
+    # @see address
+    #
     def to_s
       address.to_s
     end
 
     protected
 
-    def create_port(port)
-      state = port.at('state')
-      service = port.at('service/@name').inner_text
+    #
+    # Creates a new Port object around a node.
+    #
+    # @param [Nokogiri::XML::Node] node
+    #   The node to create the Port object around.
+    #
+    # @return [Port]
+    #   The port object.
+    #
+    def create_port(node)
+      state = node.at('state')
+      service = node.at('service/@name').inner_text
 
       return Port.new(
-        port['protocol'].to_sym,
-        port['portid'].to_i,
+        node['protocol'].to_sym,
+        node['portid'].to_i,
         state['state'].to_sym,
         state['reason'],
         service
