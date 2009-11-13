d9a44ff84ad86c0936e8650a195b5bee1fe272c0
postmodern <postmodern.mod3@gmail.com>
2009-11-12 18:50:41 -0800

    Added YARD tags to Nmap::OSClass.

--- lib/nmap/os_class.rb
+++ lib/nmap/os_class.rb
@@ -1,14 +1,33 @@
 module Nmap
   class OSClass
 
+    # The OS class
     attr_reader :type
 
+    # The OS vendor
     attr_reader :vendor
 
+    # The family of the OS
     attr_reader :family
 
+    # The accuracy of the OS guess
     attr_reader :accuracy
 
+    #
+    # Creates a new OSClass object.
+    #
+    # @param [Symbol] type
+    #   The class of the OS.
+    #
+    # @param [String] vendor
+    #   The vendor of the OS.
+    #
+    # @param [String] family
+    #   The family of the OS.
+    #
+    # @param [Integer] accuracy
+    #   The accuracy of the OS guess.
+    #
     def initialize(type,vendor,family,accuracy)
       @type = type
       @vendor = vendor
@@ -16,6 +35,12 @@ module Nmap
       @accuracy = accuracy
     end
 
+    #
+    # Converts the OS class to a String.
+    #
+    # @return [String]
+    #   The String form of the OS class.
+    #
     def to_s
       "#{@type} #{@vendor} (#{@accuracy}%)"
     end
