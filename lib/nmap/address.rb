3444d7f503b7cc3cc173ae17b77454b4c949ca35
postmodern <postmodern.mod3@gmail.com>
2009-11-12 18:31:52 -0800

    Added YARD tags to Nmap::Address.

--- lib/nmap/address.rb
+++ lib/nmap/address.rb
@@ -7,11 +7,26 @@ module Nmap
     # Address
     attr_reader :addr
 
+    #
+    # Creates a new Address object.
+    #
+    # @param [Symbol] type
+    #   The type of the address.
+    #
+    # @param [String] addr
+    #   The address.
+    #
     def initialize(type,addr)
       @type = type
       @addr = addr
     end
 
+    #
+    # Converts the address to a String.
+    #
+    # @return [String]
+    #   The address.
+    #
     def to_s
       @addr.to_s
     end
