b6f4dd8e7b9d7d19a64c8515115a234725ea54a2
postmodern <postmodern.mod3@gmail.com>
2009-11-13 01:52:24 -0800

    Added YARD tags to Nmap::Program.

--- lib/nmap/program.rb
+++ lib/nmap/program.rb
@@ -8,18 +8,45 @@ module Nmap
     name_program 'nmap'
 
     #
-    # Perform an Nmap scan using the given _options_ and _block_.
-    # If a _block_ is given, it will be passed a newly created
-    # NmapTask object.
+    # Perform an Nmap scan.
+    #
+    # @param [Hash{Symbol => Object}] options
+    #   Additional options for nmap.
+    #
+    # @yield [task]
+    #   If a block is given, it will be passed a task object
+    #   used for specifying options for nmap.
+    #
+    # @yieldparam [Task] task
+    #   The nmap task object.
+    #
+    # @return [Boolean]
+    #   Specifies whether the command exited normally.
+    #
+    # @see Task
+    # @see #scan
     #
     def self.scan(options={},&block)
       self.find.scan(options,&block)
     end
 
     #
-    # Perform an Nmap scan using the given _options_ and _block_.
-    # If a _block_ is given, it will be passed a newly created
-    # NmapTask object.
+    # Perform an Nmap scan.
+    #
+    # @param [Hash{Symbol => Object}] options
+    #   Additional options for nmap.
+    #
+    # @yield [task]
+    #   If a block is given, it will be passed a task object
+    #   used for specifying options for nmap.
+    #
+    # @yieldparam [Task] task
+    #   The nmap task object.
+    #
+    # @return [Boolean]
+    #   Specifies whether the command exited normally.
+    #
+    # @see Task
     #
     def scan(options={},&block)
       run_task(Task.new(options,&block))
