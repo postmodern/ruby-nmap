69211bbf5321d4535fa9a464f6e9b61cb4ffb0ec
postmodern <postmodern.mod3@gmail.com>
2009-11-12 18:48:28 -0800

    Added YARD tags to Nmap::OS.

--- lib/nmap/os.rb
+++ lib/nmap/os.rb
@@ -8,10 +8,28 @@ module Nmap
 
     include Enumerable
 
+    #
+    # Creates a new OS object.
+    #
+    # @param [Nokogiri::XML::Node] node
+    #   The node that contains the OS guessing information.
+    #
     def initialize(node)
       @node = node
     end
 
+    #
+    # Parses the OS class information.
+    #
+    # @yield [class]
+    #   Passes each OS class to the given block.
+    #
+    # @yieldparam [OSClass] class
+    #   The OS class information.
+    #
+    # @return [OS]
+    #   The OS information.
+    #
     def each_class(&block)
       @node.xpath("osclass").map do |osclass|
         os_class = OSClass.new(
@@ -27,10 +45,28 @@ module Nmap
       return self
     end
 
+    #
+    # Parses the OS class information.
+    #
+    # @return [Array<OSClass>]
+    #   The OS class information.
+    #
     def classes
       Enumerator.new(self,:each_class).to_a
     end
 
+    #
+    # Parses the OS match information.
+    #
+    # @yield [match]
+    #   Passes each OS match to the given block.
+    #
+    # @yieldparam [OSMatch] class
+    #   The OS match information.
+    #
+    # @return [OS]
+    #   The OS information.
+    #
     def each_match(&block)
       @node.xpath("osmatch").map do |osclass|
         os_match = OSMatch.new(
@@ -44,18 +80,41 @@ module Nmap
       return self
     end
 
+    #
+    # Parses the OS match information.
+    #
+    # @return [Array<OSMatch>]
+    #   The OS match information.
+    #
     def matches
       Enumerator.new(self,:each_match).to_a
     end
 
+    #
+    # Parses the ports used for guessing the OS.
+    #
+    # @return [Array<Integer>]
+    #   The ports used.
+    #
     def ports_used
       @node.xpath("portused/@portid").map { |port| port.inner_text.to_i }
     end
 
+    #
+    # Parses the OS fingerprint used by Nmap.
+    #
+    # @return [String]
+    #   The OS fingerprint.
+    #
     def fingerprint
       @node.at("osfingerprint/@fingerprint").inner_text
     end
 
+    #
+    # Parses the OS match information.
+    #
+    # @see each_match
+    #
     def each(&block)
       each_match(&block)
     end
