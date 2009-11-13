4f6be235be81993b9cdcadb1a329098e0b662c06
postmodern <postmodern.mod3@gmail.com>
2009-11-12 18:52:41 -0800

    Added YARD tags to Nmap::OSMatch.

--- lib/nmap/os_match.rb
+++ lib/nmap/os_match.rb
@@ -1,15 +1,32 @@
 module Nmap
   class OSMatch
 
+    # The name of the OS
     attr_reader :name
 
+    # The accuracy of the OS guess
     attr_reader :accuracy
 
+    #
+    # Creates a OSMatch object.
+    #
+    # @param [String] name
+    #   The name of the OS.
+    #
+    # @param [Integer] accuracy
+    #   The accuracy of the OS guess.
+    #
     def initialize(name,accuracy)
       @name = name
       @accuracy = accuracy
     end
 
+    #
+    # Converts the OS match to a String.
+    #
+    # @return [String]
+    #   The String form of the OS match.
+    #
     def to_s
       "#{@name} (#{@accuracy}%)"
     end
