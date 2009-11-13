f1af22771f1ebdced6e13f07765994d9e6c1e626
postmodern <postmodern.mod3@gmail.com>
2009-11-12 18:57:12 -0800

    Added YARD tags to Nmap::Scan.

--- lib/nmap/scan.rb
+++ lib/nmap/scan.rb
@@ -1,18 +1,39 @@
 module Nmap
   class Scan
 
+    # The type of scan
     attr_reader :type
 
+    # The protocol the scan entailed
     attr_reader :protocol
 
+    # The port numbers that were scanned
     attr_reader :services
 
+    #
+    # Creates a new Scan object.
+    #
+    # @param [Symbol] type
+    #   The type of the scan.
+    #
+    # @param [Symbol] protocol
+    #   The protocol the scan used.
+    #
+    # @param [Array<Integer, Rage>] services
+    #   The port numbers scanned.
+    #
     def initialize(type,protocol,services=[])
       @type = type
       @protocol = protocol
       @services = services
     end
 
+    #
+    # Converts the scan to a String.
+    #
+    # @return [String]
+    #   The String form of the scan.
+    #
     def to_s
       "#{@protocol} #{@type}"
     end
