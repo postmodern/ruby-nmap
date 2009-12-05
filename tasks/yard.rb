46488a064a43bc106b856bb8b8352146e677f8b2
postmodern <postmodern.mod3@gmail.com>
2009-12-04 17:40:39 -0800

    No need to add the lib/ directory to $LOAD_PATH.

--- tasks/yard.rb
+++ tasks/yard.rb
@@ -1,8 +1,3 @@
-lib_dir = File.expand_path(File.join(File.dirname(__FILE__),'..','lib'))
-unless $LOAD_PATH.include?(lib_dir)
-  $LOAD_PATH.unshift(lib_dir)
-end
-
 require 'yard'
 
 YARD::Rake::YardocTask.new do |t|
