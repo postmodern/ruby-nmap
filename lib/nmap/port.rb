ba004ec30b7aa1402ee1e71716a1653547f39dc2
Postmodern <postmodern.mod3@gmail.com>
2010-11-08 20:05:50 -0800

    Added missing YARD tags.

--- lib/nmap/port.rb
+++ lib/nmap/port.rb
@@ -11,27 +11,52 @@ module Nmap
       @node = node
     end
 
+    #
     # The protocol the port runs on
+    #
+    # @return [Symbol]
+    #   The protocol of the port.
+    #
     def protocol
       @protocol ||= @node['protocol'].to_sym
     end
 
-    # The port number
+    #
+    # The port number.
+    #
+    # @return [Integer]
+    #   The number of the port.
+    #
     def number
       @number ||= @node['portid'].to_i
     end
 
-    # The state of the port
+    #
+    # The state of the port.
+    #
+    # @return [Symbol]
+    #   The state of the port (`:open`, `:filtered` or `:closed`).
+    #
     def state
       @state ||= @node.at('state/@state').inner_text.to_sym
     end
 
-    # The reason the port was discovered
+    #
+    # The reason the port was discovered.
+    #
+    # @return [String]
+    #   How the port was discovered.
+    #
     def reason
       @reason ||= @node.at('state/@reason').inner_text
     end
 
-    # The service the port provides
+    #
+    # The service the port provides.
+    #
+    # @return [String]
+    #   The service detected on the port.
+    #
     def service
       @service ||= if (service = @node.at('service/@name'))
                      service.inner_text
