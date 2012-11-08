204f72a95ad35eff560458c1dd73d3627d977375
Postmodern <postmodern.mod3@gmail.com>
2012-11-07 21:29:59 -0800

    More Struct.

--- lib/nmap/os_class.rb
+++ lib/nmap/os_class.rb
@@ -1,39 +1,8 @@
 module Nmap
-  class OSClass
-
-    # The OS class
-    attr_reader :type
-
-    # The OS vendor
-    attr_reader :vendor
-
-    # The family of the OS
-    attr_reader :family
-
-    # The accuracy of the OS guess
-    attr_reader :accuracy
-
-    #
-    # Creates a new OSClass object.
-    #
-    # @param [Symbol] type
-    #   The class of the OS.
-    #
-    # @param [String] vendor
-    #   The vendor of the OS.
-    #
-    # @param [String] family
-    #   The family of the OS.
-    #
-    # @param [Integer] accuracy
-    #   The accuracy of the OS guess.
-    #
-    def initialize(type,vendor,family,accuracy)
-      @type = type
-      @vendor = vendor
-      @family = family
-      @accuracy = accuracy
-    end
+  #
+  # Represents an OS class.
+  #
+  class OSClass < Struct.new(:type, :vendor, :family, :accuracy)
 
     #
     # Converts the OS class to a String.
@@ -42,7 +11,7 @@ module Nmap
     #   The String form of the OS class.
     #
     def to_s
-      "#{@type} #{@vendor} (#{@accuracy}%)"
+      "#{self.type} #{self.vendor} (#{self.accuracy}%)"
     end
 
   end
