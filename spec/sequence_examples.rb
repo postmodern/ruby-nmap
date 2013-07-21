7b52b5b8c8e2846aacf077fafd1877b54b7ae187
Postmodern <postmodern.mod3@gmail.com>
2013-07-20 21:19:58 -0700

    Use the have().items matcher.

--- spec/sequence_examples.rb
+++ spec/sequence_examples.rb
@@ -4,10 +4,7 @@ shared_examples_for "Sequence" do
   describe "#values" do
     subject { super().values }
 
-    it "should have length of 6" do
-      subject.length.should == 6
-    end
-
+    it { should have(6).items }
     it { should all_be_between(0,0xFFFFFFFF) }
   end
 end
