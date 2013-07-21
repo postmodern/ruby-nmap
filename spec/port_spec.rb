80d71d49b5afcf7dbb8ac532cb1dfd1a8f53c13e
Postmodern <postmodern.mod3@gmail.com>
2013-07-20 20:26:16 -0700

    Updated Port#scripts specs.

--- spec/port_spec.rb
+++ spec/port_spec.rb
@@ -26,26 +26,18 @@ describe Port do
     it "should return a Service object" do
       subject.should be_kind_of(Service)
     end
+  end
 
-    it "should parse the service name" do
-      subject.name.should == 'ssh'
-    end
+  describe "#scripts" do
+    subject { super().scripts }
+
+    it { should_not be_empty     }
+    it { should be_kind_of(Hash) }
   end
 
   describe "#inspect" do
     it "should include the number" do
       subject.inspect.should include(subject.number.to_s)
-    end
-  end
-
-  context "when NSE scripts are ran" do
-    let(:xml) { XML.new(Helpers::NSE_FILE) }
-
-    it "should parse the output of scripts" do
-      subject.scripts.should_not be_empty
-
-      subject.scripts.keys.should_not include(nil)
-      subject.scripts.values.should_not include(nil)
     end
   end
 end
