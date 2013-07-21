20414bb53510da2a479268ad95f58a52e2a1b52d
Postmodern <postmodern.mod3@gmail.com>
2013-07-20 20:10:46 -0700

    Use the new scan.xml.

--- spec/ip_id_sequence_spec.rb
+++ spec/ip_id_sequence_spec.rb
@@ -2,19 +2,17 @@ require 'spec_helper'
 require 'nmap/ip_id_sequence'
 
 describe IpIdSequence do
-  let(:xml) { XML.new(Helpers::SCAN_FILE) }
-
-  subject { xml.hosts.first.ip_id_sequence }
+  subject { @xml.hosts.first.ip_id_sequence }
 
   it "should be accessible from host objects" do
     subject.should be_kind_of(IpIdSequence)
   end
 
   it "should parse the description" do
-    subject.description.should == "Incremental"
+    subject.description.should == "All zeros"
   end
 
   it "should parse the values" do
-    subject.values.should == [0x1FB0, 0x1FB2, 0x1FB4, 0x1FB6, 0x1FB8, 0x1FBA]
+    subject.values.should == [0, 0, 0, 0, 0, 0]
   end
 end
