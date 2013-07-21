aacf5e73bc00923ddeac907aa0b018aad256c968
Postmodern <postmodern.mod3@gmail.com>
2013-07-20 20:20:16 -0700

    Updated the OS specs.

--- spec/os_spec.rb
+++ spec/os_spec.rb
@@ -4,37 +4,47 @@ require 'nmap/os'
 require 'cgi'
 
 describe OS do
-  let(:xml) { XML.new(Helpers::SCAN_FILE) }
+  subject { @xml.hosts.first.os }
 
-  subject { xml.hosts.first.os }
+  describe "#classes" do
+    subject { super().classes.first }
 
-  it "should parse the OS classes" do
-    classes = subject.classes
+    it "should parse the type" do
+      subject.type.should == :"general purpose"
+    end
 
-    classes.length.should == 1
-    classes[0].type.should == :WAP
-    classes[0].vendor.should == 'Netgear'
-    classes[0].family.should == :embedded
-    classes[0].accuracy.should == 100
+    it "should parse the vendor" do
+      subject.vendor.should == 'Linux'
+    end
+
+    it "should parse the family" do
+      subject.family.should == :Linux
+    end
+
+    it "should parse the accuracy" do
+      subject.accuracy.should be_between(0,100)
+    end
   end
 
-  it "should parse the OS matches" do
-    matches = subject.matches
+  describe "#matches" do
+    subject { super().matches.first }
 
-    matches.length.should == 2
+    it "should parse the name" do
+      subject.name.should == 'Linux 2.6.39'
+    end
 
-    matches[0].name.should == 'Netgear WGR614v6 wireless broadband router'
-    matches[0].accuracy.should == 100
-
-    matches[1].name.should == 'Netgear WGR614v7 or WPN824v2 wireless broadband router'
-    matches[1].accuracy.should == 100
+    it "should parse the accuracy" do
+      subject.accuracy.should be_between(0,100)
+    end
   end
 
   it "should parse the ports used" do
-    subject.ports_used.should == [443, 21]
+    subject.ports_used.should == [22, 1, 31542]
   end
 
   it "should parse the OS fingerprints" do
-    subject.fingerprint.should == CGI.unescapeHTML("SCAN(V=4.68%D=8/16%OT=443%CT=21%CU=%PV=Y%DS=1%G=N%M=001D7E%TM=48A77607%P=i686-pc-linux-gnu)&#xa;SEQ(SP=19%GCD=FA00%ISR=9E%TI=I%TS=1)&#xa;OPS(O1=M5B4NW0NNT11%O2=M5B4NW0NNT11%O3=M5B4NW0NNT11%O4=M5B4NW0NNT11%O5=M5B4NW0NNT11%O6=M5B4NNT11)&#xa;WIN(W1=2000%W2=2000%W3=2000%W4=2000%W5=2000%W6=2000)&#xa;ECN(R=Y%DF=N%TG=40%W=2000%O=M5B4NW0%CC=N%Q=)&#xa;T1(R=Y%DF=N%TG=40%S=O%A=S+%F=AS%RD=0%Q=)&#xa;T2(R=N)&#xa;T3(R=N)&#xa;T4(R=N)&#xa;T5(R=Y%DF=N%TG=40%W=0%S=Z%A=S+%F=AR%O=%RD=0%Q=)&#xa;T6(R=N)&#xa;T7(R=N)&#xa;U1(R=N)&#xa;IE(R=N)&#xa;")
+    pending "scan.xml does not currently have an osfingerprint" do
+      subject.fingerprint.should == CGI.unescapeHTML("SCAN(V=4.68%D=8/16%OT=443%CT=21%CU=%PV=Y%DS=1%G=N%M=001D7E%TM=48A77607%P=i686-pc-linux-gnu)&#xa;SEQ(SP=19%GCD=FA00%ISR=9E%TI=I%TS=1)&#xa;OPS(O1=M5B4NW0NNT11%O2=M5B4NW0NNT11%O3=M5B4NW0NNT11%O4=M5B4NW0NNT11%O5=M5B4NW0NNT11%O6=M5B4NNT11)&#xa;WIN(W1=2000%W2=2000%W3=2000%W4=2000%W5=2000%W6=2000)&#xa;ECN(R=Y%DF=N%TG=40%W=2000%O=M5B4NW0%CC=N%Q=)&#xa;T1(R=Y%DF=N%TG=40%S=O%A=S+%F=AS%RD=0%Q=)&#xa;T2(R=N)&#xa;T3(R=N)&#xa;T4(R=N)&#xa;T5(R=Y%DF=N%TG=40%W=0%S=Z%A=S+%F=AR%O=%RD=0%Q=)&#xa;T6(R=N)&#xa;T7(R=N)&#xa;U1(R=N)&#xa;IE(R=N)&#xa;")
+    end
   end
 end
