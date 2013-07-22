22344f9f5aab4be34752de0e45b25af2534c2d90
Postmodern <postmodern.mod3@gmail.com>
2013-07-21 20:41:36 -0700

    Added XML#each_task.

--- lib/nmap/xml.rb
+++ lib/nmap/xml.rb
@@ -161,22 +161,44 @@ module Nmap
     #
     # Parses the tasks of the scan.
     #
-    # @return [Array<ScanTask>]
-    #   The tasks of the scan.
+    # @yield [task]
+    #   The given block will be passed each scan task.
     #
-    # @since 0.1.2
+    # @yieldparam [ScanTask] task
+    #   A task from the scan.
     #
-    def tasks
-      @doc.xpath('/nmaprun/taskbegin').map do |task_begin|
+    # @return [Enumerator]
+    #   If no block is given, an enumerator will be returned.
+    #
+    # @since 0.7.0
+    #
+    def each_task
+      return enum_for(__method__) unless block_given?
+
+      @doc.xpath('/nmaprun/taskbegin').each do |task_begin|
         task_end = task_begin.xpath('following-sibling::taskend').first
 
-        ScanTask.new(
+        yield ScanTask.new(
           task_begin['task'],
           Time.at(task_begin['time'].to_i),
           Time.at(task_end['time'].to_i),
           task_end['extrainfo']
         )
       end
+
+      return self
+    end
+
+    #
+    # Parses the tasks of the scan.
+    #
+    # @return [Array<ScanTask>]
+    #   The tasks of the scan.
+    #
+    # @since 0.1.2
+    #
+    def tasks
+      each_task.to_a
     end
 
     #
