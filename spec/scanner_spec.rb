4fefe8fbd958c3b2b70f4803a0690da88b14b34d
Postmodern <postmodern.mod3@gmail.com>
2013-07-21 20:38:51 -0700

    Added spec for Scanner#to_s (closes #17).

---
+++ spec/scanner_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require 'nmap/scanner'
+
+describe Scanner do
+  describe "#to_s" do
+    let(:name)    { 'nmap' }
+    let(:version) { '6.01' }
+    let(:args)    { 'nmap -v -sS -sU -A -O -oX spec/scan.xml scanme.nmap.org' }
+    let(:start_time) { Time.new('Sat Jul 20 23:55:27 2013') }
+
+    subject do
+      described_class.new(
+        name,
+        version,
+        args,
+        start_time
+      )
+    end
+
+    it "should return the scanner command" do
+      subject.to_s.should == "#{name} #{args}"
+    end
+  end
+end
