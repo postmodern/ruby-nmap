4b16e1da3137e4c3c3344766fd6c72133e9be14d
Postmodern <postmodern.mod3@gmail.com>
2013-07-30 19:58:01 -0700

    Spec style.

--- spec/ip_id_sequence_spec.rb
+++ spec/ip_id_sequence_spec.rb
@@ -6,12 +6,11 @@ require 'nmap/ip_id_sequence'
 describe IpIdSequence do
   subject { @xml.hosts.first.ip_id_sequence }
 
-  it "should parse the description" do
-    subject.description.should == "All zeros"
+  describe "#description" do
+    it "should parse the description" do
+      subject.description.should == "All zeros"
+    end
   end
 
   it_should_behave_like "Sequence"
-
-  describe "#to_s" do
-  end
 end
