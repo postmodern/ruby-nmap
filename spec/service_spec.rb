f782d9ca2c99325a57b9b395bdccd39d249fd8c5
Postmodern <postmodern.mod3@gmail.com>
2013-08-03 18:08:30 -0700

    Added more specs for Service.
    * Added pending specs for the data we are missing.

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -7,24 +7,70 @@ require 'nmap/service'
 describe Service do
   subject { @xml.hosts.first.ports.first.service }
 
-  it "should parse the name" do
-    subject.name.should == 'ssh'
+  describe "#name" do
+    it "should parse the name" do
+      subject.name.should == 'ssh'
+    end
   end
 
-  it "should parse the fingerprint method" do
-    subject.fingerprint_method.should == :probed
+  describe "#ssl?" do
+    it "should check the tunnel attribute" do
+      pending "need a service that uses SSL"
+    end
   end
 
-  it "should parse the confidence" do
-    subject.confidence.should be_between(0,10)
+  describe "#protocol" do
+    it "should parse the proto attribute" do
+      pending "need a service with the proto attribute"
+    end
   end
 
-  it "should parse the product name" do
-    subject.product.should == 'OpenSSH'
+  describe "#product" do
+    it "should parse the product name attribute" do
+      subject.product.should == 'OpenSSH'
+    end
   end
 
-  it "should parse the version" do
-    subject.version.should == '5.3p1 Debian 3ubuntu7'
+  describe "#version" do
+    it "should parse the version attribute" do
+      subject.version.should == '5.3p1 Debian 3ubuntu7'
+    end
+  end
+
+  describe "#extra_info" do
+    it "should parse the extrainfo attribute" do
+      subject.extra_info.should == 'protocol 2.0'
+    end
+  end
+
+  describe "#hostname" do
+    it "should parse the hostname attribute" do
+      pending "need a service with the hostname attribute"
+    end
+  end
+
+  describe "#os_type" do
+    it "should parse the ostype attribute" do
+      subject.os_type.should == 'Linux'
+    end
+  end
+
+  describe "#device_type" do
+    it "should parse the devicetype attribute" do
+      pending "need a service with the devicetype attribute"
+    end
+  end
+
+  describe "#fingerprint_method" do
+    it "should parse the method attribute" do
+      subject.fingerprint_method.should == :probed
+    end
+  end
+
+  describe "#confidence" do
+    it "should parse the conf attribute" do
+      subject.confidence.should be_between(0,10)
+    end
   end
 
   it_should_behave_like "CPE"
