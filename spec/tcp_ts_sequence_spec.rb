6245d90dbb2f1b0718a0a836ae166171bffbf83f
Postmodern <postmodern.mod3@gmail.com>
2013-08-03 17:42:01 -0700

    Added a spec for TCPTSSequence#to_s.

--- spec/tcp_ts_sequence_spec.rb
+++ spec/tcp_ts_sequence_spec.rb
@@ -6,8 +6,28 @@ require 'nmap/tcp_ts_sequence'
 describe TcpTsSequence do
   subject { @xml.hosts.first.tcp_ts_sequence }
 
-  it "should parse the description" do
-    subject.description.should == "1000HZ"
+  describe "#description" do
+    it "should parse the description" do
+      subject.description.should == "1000HZ"
+    end
+  end
+
+  describe "#to_s" do
+    let(:description_regexp) do
+      /"[^"]+"/
+    end
+
+    let(:values_regexp) do
+      /\[\d{9}(, \d{9}){5}\]/
+    end
+
+    let(:regexp) do
+      /^description=#{description_regexp} values=#{values_regexp}$/
+    end
+
+    it "should contain the description and values" do
+      subject.to_s.should =~ regexp
+    end
   end
 
   it_should_behave_like "Sequence"
