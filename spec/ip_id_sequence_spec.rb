43c32383f9a9e072728714d1630a4738648b1433
Postmodern <postmodern.mod3@gmail.com>
2013-08-03 17:58:26 -0700

    Added a spec for IpIdSequence#to_s.

--- spec/ip_id_sequence_spec.rb
+++ spec/ip_id_sequence_spec.rb
@@ -12,5 +12,23 @@ describe IpIdSequence do
     end
   end
 
+  describe "#to_s" do
+    let(:description_regexp) do
+      /"[^"]+"/
+    end
+
+    let(:values_regexp) do
+      /\[\d+(, \d+){5}\]/
+    end
+
+    let(:regexp) do
+      /^description=#{description_regexp} values=#{values_regexp}$/
+    end
+
+    it "should contain the description and values" do
+      subject.to_s.should =~ regexp
+    end
+  end
+
   it_should_behave_like "Sequence"
 end
