48fd3e51de6e23c66f50cc8a4f48dfa5ad20ccdf
Postmodern <postmodern.mod3@gmail.com>
2014-08-13 16:47:49 -0700

    Use the built-in be_between matcher.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,12 +6,6 @@ require 'nmap/version'
 require 'nmap/xml'
 include Nmap
 
-RSpec::Matchers.define :be_between do |min,max|
-  match do |value|
-    (value >= min) && (value <= max)
-  end
-end
-
 RSpec::Matchers.define :all_be_between do |min,max|
   match do |values|
     values.all? { |value| (value >= min) && (value <= max) }
