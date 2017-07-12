75b02680337567f99a11fd54c6e24fa076ded280
Jamil Bou Kheir <jaboukhe@cisco.com>
2017-07-12 09:34:19 -0700

    Support port / proto ranges passed to ping types

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -12,25 +12,89 @@ describe Task do
     it "should format a String of ports" do
       subject.ports = '80,21,25'
 
-      expect(subject.arguments).to eq(%w[-p 80,21,25])
+      expect(subject.arguments).to eq(['-p80,21,25'])
     end
 
     it "should format an Array of String ports" do
       subject.ports = %w[80 21 25]
 
-      expect(subject.arguments).to eq(%w[-p 80,21,25])
+      expect(subject.arguments).to eq(['-p80,21,25'])
     end
 
     it "should format an Array of Integer ports" do
       subject.ports = [80, 21, 25]
 
-      expect(subject.arguments).to eq(%w[-p 80,21,25])
+      expect(subject.arguments).to eq(['-p80,21,25'])
     end
 
     it "should format a Range of ports" do
       subject.ports = [80, 21..25]
 
-      expect(subject.arguments).to eq(%w[-p 80,21-25])
+      expect(subject.arguments).to eq(['-p80,21-25'])
+    end
+  end
+
+  describe "#syn_discovery=" do
+    it "should ignore empty port Arrays" do
+      subject.syn_discovery = []
+
+      expect(subject.arguments).to eq([])
+    end
+
+    it "should format a String of ports" do
+      subject.syn_discovery = '80,21,25'
+
+      expect(subject.arguments).to eq(%w[-PS80,21,25])
+    end
+
+    it "should format an Array of String ports" do
+      subject.syn_discovery = %w[80 21 25]
+
+      expect(subject.arguments).to eq(%w[-PS80,21,25])
+    end
+
+    it "should format an Array of Integer ports" do
+      subject.syn_discovery = [80, 21, 25]
+
+      expect(subject.arguments).to eq(%w[-PS80,21,25])
+    end
+
+    it "should format a Range of ports" do
+      subject.syn_discovery = [80, 21..25]
+
+      expect(subject.arguments).to eq(%w[-PS80,21-25])
+    end
+
+    it "should use default port if none specified" do
+      subject.syn_discovery = true
+
+      expect(subject.arguments).to eq(%w[-PS])
+    end
+  end
+
+  describe "#ip_ping=" do
+    it "should ignore empty proto Arrays" do
+      subject.ip_ping = []
+
+      expect(subject.arguments).to eq([])
+    end
+
+    it "should format a String of protos" do
+      subject.ip_ping = 'ICMP,IGMP,TCP'
+
+      expect(subject.arguments).to eq(%w[-POICMP,IGMP,TCP])
+    end
+
+    it "should format an Array of String protos" do
+      subject.ip_ping = %w[ICMP IGMP TCP]
+
+      expect(subject.arguments).to eq(%w[-POICMP,IGMP,TCP])
+    end
+
+    it "should use default port if none specified" do
+      subject.ip_ping = true
+
+      expect(subject.arguments).to eq(%w[-PO])
     end
   end
 end
