e43a95a1963c43ac1fc663dd9bfbce3badd234e8
Postmodern <postmodern.mod3@gmail.com>
2018-04-30 14:13:31 -0700

    Added Task#defeat_icmp_ratelimit for the `--defeat-icmp-ratelimit` option.

--- lib/nmap/task.rb
+++ lib/nmap/task.rb
@@ -96,6 +96,7 @@ module Nmap
   # * `--min-rate` - `nmap.min_rate`
   # * `--max-rate` - `nmap.max_rate`
   # * `--defeat-rst-ratelimit` - `nmap.defeat_rst_ratelimit`
+  # * `--defeat-icmp-ratelimit` - `nmap.defeat_icmp_ratelimit`
   # * `-T` - `nmap.timing_template`
   # * `-T0` - `nmap.paranoid_timing`
   # * `-T1` - `nmap.sneaky_timing`
@@ -267,6 +268,7 @@ module Nmap
     long_option :flag => '--min-rate'
     long_option :flag => '--max-rate'
     long_option :flag => '--defeat-rst-ratelimit'
+    long_option :flag => '--defeat-icmp-ratelimit'
     short_option :flag => '-T', :name => :timing_template
     short_option :flag => '-T0', :name => :paranoid_timing
     short_option :flag => '-T1', :name => :sneaky_timing
