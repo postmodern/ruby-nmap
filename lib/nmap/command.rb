cd71d75c52acdd6e5cbccebeeba869d367d7bf18
Postmodern <postmodern.mod3@gmail.com>
2022-11-06 09:57:53 -0800

    Added the `--script-args-file` option (closes #64).

--- lib/nmap/command.rb
+++ lib/nmap/command.rb
@@ -75,6 +75,7 @@ module Nmap
   # * `-sC` - `nmap.default_script`
   # * `--script` - `nmap.script`
   # * `--script-args` - `nmap.script_args`
+  # * `--script-args-file` - `nmap.script_args_file`
   # * `--script-help` - `nmap.script_help`
   # * `--script-trace` - `nmap.script_trace`
   # * `--script-updatedb` - `nmap.update_scriptdb`
@@ -474,6 +475,7 @@ module Nmap
       option '-sC', name: :default_script
       option '--script', value: {type: List.new}
       option '--script-args', value: {type: KeyValueList.new}
+      option '--script-args-file', value: {type: InputFile.new}
       option '--script-help', value: {type: List.new}
       option '--script-trace'
       option '--script-updatedb', name: :update_scriptdb
