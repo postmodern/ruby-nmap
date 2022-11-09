dc493bc9ca11ae7abd1b4b9b7fb585bfa9593c05
Postmodern <postmodern.mod3@gmail.com>
2022-11-09 03:39:05 -0800

    Renamed `nmap.xml` to `nmap.output_xml` (closes #81).

--- lib/nmap/command.rb
+++ lib/nmap/command.rb
@@ -135,7 +135,7 @@ module Nmap
   # ### Output:
   #
   # * `-oN path/to/file` - `nmap.save = "path/to/file"`
-  # * `-oX path/to/file` - `nmap.xml = "path/to/file"`
+  # * `-oX path/to/file` - `nmap.output_xml = "path/to/file"`
   # * `-oS path/to/file` - `nmap.output_skiddie = "path/to/file"`
   # * `-oG path/to/file` - `nmap.output_grepable = "path/to/file"`
   # * `-oA path/to/basename` - `nmap.output_all = "path/to/basename"`
@@ -715,7 +715,7 @@ module Nmap
 
       # OUTPUT:
       option '-oN', name: :save, value: true
-      option '-oX', name: :xml, value: true
+      option '-oX', name: :output_xml, value: true
       option '-oS', name: :output_skiddie, value: true
       option '-oG', name: :output_grepable, value: true
       option '-oA', name: :output_all, value: true
